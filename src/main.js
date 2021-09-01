1a1e0ec1f0e97c7785783210bd84c6b88cc62b00
liuwei <1097408921@qq.com>
2021-09-01 08:00:56 +0800

    引入mint-ui,修改退出登录按钮

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 */
 
 import Vue from 'vue'
+import {Button} from 'mint-ui'
 import App from './App'
 import router from './router'
 import store from './store'
+
+Vue.component(Button.name, Button)
 
 new Vue({
   el: '#app',
